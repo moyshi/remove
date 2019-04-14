b6e021bfa8dd6aeeb825a0deec09db982100740e
moyshi <46315868+moyshi@users.noreply.github.com>
2019-04-14 12:38:53 +0300

    Add files via upload

---
+++ remove.py
@@ -0,0 +1,22 @@
+import cv2
+import numpy as np
+
+img = cv2.imread('4C12D970-95F5-4C2B-AACF-6E40413F38B8.jpg')
+
+rgb_planes = cv2.split(img)
+
+result_planes = []
+result_norm_planes = []
+for plane in rgb_planes:
+    dilated_img = cv2.dilate(plane, np.ones((7, 7), np.uint8))
+    bg_img = cv2.medianBlur(dilated_img, 21)
+    diff_img = 255 - cv2.absdiff(plane, bg_img)
+    norm_img = cv2.normalize(diff_img, plane, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX)
+    result_planes.append(diff_img)
+    result_norm_planes.append(norm_img)
+
+result = cv2.merge(result_planes)
+result_norm = cv2.merge(result_norm_planes)
+
+cv2.imwrite('shadows_out.png', result)
+cv2.imwrite('shadows_out_norm.png', result_norm)
